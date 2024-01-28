77fb20c647bbdbfaf872cf2af2abd1f561c1a29b
Tony Yang <tony0x00@gmail.com>
2024-01-28 11:37:34 -0500

    add icon

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,14 +1,24 @@
 import "./Projects.scss";
-
+import GooglePlayIcon from "assets/google-play-badge.png";
 const Projects = () => {
   return (
     <section id="project-section">
       <h1>Projects</h1>
-      <div className="card">
-        <h3>Ultimate Clicker</h3>
-        <img src="react-pages/ultimate_clicker_logo.png" />
-        <div className="card-container">
-          <p>Clicker APP</p>
+      <div className="container">
+        <div className="card">
+          <div className="card-container">
+            <h3>Ultimate Clicker</h3>
+            <img src="react-pages/ultimate_clicker_logo.png" />
+            <img className="google-play-icon" src={GooglePlayIcon} />
+            <p>Clicker APP</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-container">
+            <h3>Ultimate Clicker</h3>
+            <img src="react-pages/ultimate_clicker_logo.png" />
+            <p>Clicker APP</p>
+          </div>
         </div>
       </div>
     </section>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "react-pages",
+  base: "/react-pages",
   plugins: [react()],
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+    },
+  },
 });
